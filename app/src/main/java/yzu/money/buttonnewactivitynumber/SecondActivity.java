8451a7561fd502a2bdd73a242408548fbe7bbbb5
howdyhi <money@AnonymousdeMacBook-Air.local>
2016-10-30 16:32:40 +0800

    set SecondActivity

--- app/src/main/java/yzu/money/buttonnewactivitynumber/SecondActivity.java
+++ app/src/main/java/yzu/money/buttonnewactivitynumber/SecondActivity.java
@@ -2,12 +2,38 @@ package yzu.money.buttonnewactivitynumber;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+    private LinearLayout ll;
+    private TextView txtView;
+    private Button backBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        ll= (LinearLayout) findViewById(R.id.activity_second);
+
+        txtView = new TextView(this);
+        //if(se)
+        ll.addView(txtView);
+
+        backBtn = new Button(this);
+        backBtn.setText(R.string.backBtn_name);
+        ll.addView(backBtn);
+        backBtn.setOnClickListener(new Button.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        //backBtn.setOnClickListener((v));
+
     }
 }
